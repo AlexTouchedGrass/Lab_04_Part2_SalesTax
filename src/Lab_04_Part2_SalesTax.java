9853e5c47f183b7313d202df1fefb7be5ac85ec6
AlexTouchedGrass <dean-potts.622237@swl.k12.oh.us>
2023-09-15 09:26:26 -0400

    Words

--- src/Lab_04_Part2_SalesTax.java
+++ src/Lab_04_Part2_SalesTax.java
@@ -1,5 +1,19 @@
-public class Main {
+import java.util.Scanner;
+
+public class Lab_04_Part2_SalesTax {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        double doublePrice;
+        double doubleTax = 1.05;
+        double doubleTotal;
+
+        System.out.println("What is the price of your item?");
+
+        doublePrice = scan.nextDouble();
+
+        doubleTotal = doublePrice * doubleTax;
+
+        System.out.println("The price of your item, including tax, is $" + doubleTotal + ".");
     }
 }
